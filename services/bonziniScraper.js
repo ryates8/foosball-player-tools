90aac12b359ae851baee79e40430b07b2a53a4f9
Robert Yates <rob.yates8@gmail.com>
2016-07-30 13:53:35 -0400

    caching array lengths for our loops

--- services/bonziniScraper.js
+++ services/bonziniScraper.js
@@ -4,46 +4,17 @@ function parseHtml(html, callback) {
     //This function converts html to an array of player objects
     var htmlparser = require('htmlparser');
     var rawHtml = html;
-    var playersArray = [];
 
     var handler = new htmlparser.DefaultHandler(function(error, dom) {
         if (error) {
             callback(error);
         } else {
-            var onlyRows = [];
-            var table = dom[0].children[3];
-            var tableElements = table.children[1].children;
-
-            //Loop through list of tableElements, and only add the table rows
-            for (var z = 0; z < tableElements.length; z++) {
-                if (tableElements[z].name === 'tr') {
-                    onlyRows.push(tableElements[z]);
-                }
-            }
-
-            //Loop through only rows and save all td's that arent undefined
-            for (var x = 0; x < onlyRows.length; x++) {
-                var tds = onlyRows[x].children;
-                var tempArray = [];
-                for (var i = 0; i < tds.length; i++) {
-
-                    if (tds[i].name === 'td' && tds[i].children !== undefined) {
-
-
-                        tempArray.push(tds[i].children[0].data);
-
-                    }
-                }
-                if (tempArray.length > 0) {
-                    playersArray.push(new player(tempArray));
-                }
-            }
-            //return an object containing the parsed players data
-            callback(false, { tableHeaders: playersArray.shift(), tableData: playersArray });
+            var tableRows = getTableRows(dom);
+            var arrayOfPlayers = getPlayers(tableRows);
+            callback(false, { tableHeaders: arrayOfPlayers.shift(), tableData: arrayOfPlayers });
         }
 
     });
-
     var parser = new htmlparser.Parser(handler);
     parser.parseComplete(rawHtml);
 }
@@ -90,6 +61,45 @@ function player(csvRow) {
     }
 
 
+}
+
+function getTableRows(dom) {
+    var onlyRows = [];
+    var table = dom[0].children[3];
+    var tableElements = table.children[1].children;
+    var elementsLength = tableElements.length;
+
+    //Loop through list of tableElements, and only add the table rows
+    for (var z = 0; z < elementsLength; z++) {
+        if (tableElements[z].name === 'tr') {
+            onlyRows.push(tableElements[z]);
+        }
+    }
+
+    return onlyRows;
+}
+
+function getPlayers(rows) {
+    var playersArray = [];
+    var rowLength = rows.length;
+    //Loop through only rows and save all td's that arent undefined
+    for (var x = 0; x < rowLength; x++) {
+        var tds = rows[x].children;
+        var tdsLength = tds.length;
+        var tempArray = [];
+        for (var i = 0; i < tdsLength; i++) {
+
+            if (tds[i].name === 'td' && tds[i].children !== undefined) {
+
+                tempArray.push(tds[i].children[0].data);
+
+            }
+        }
+        if (tempArray.length > 0) {
+            playersArray.push(new player(tempArray));
+        }
+    }
+    return playersArray;
 }
 
 module.exports.init = function(callback) {
